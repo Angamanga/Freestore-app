3b8205025bb83904281192599a13ffc175152c6a
Angamanga <Angamanga@annas-mbp.lan>
2015-04-17 16:17:00 +0200

    fileupload funkar hemma

--- Prototypes/4/app/routes.js
+++ Prototypes/4/app/routes.js
@@ -1,17 +1,21 @@
 var bodyParser = require('body-parser');
+var multer = require('multer');
 
 module.exports = function(app,db){
+    //definierar collection things
     
     var things = db.collection('things');
-    
+    //visar index när man gör en get av /
     app.get('/',function(req,res){
        res.render('index');
     });
     
+    //visar newThing när man gör en get av /nysak
     app.get('/nysak',function(req,res){
         res.render('newThing');
     });
 
+    //lägger till en ny sak i databasen när man gör en post på /nysak
     app.post('/nysak',function(req,res){
          var newThing = {
             title:req.body.title,
@@ -22,11 +26,23 @@ module.exports = function(app,db){
                 res.send(err);
             }
             else{
+                //visar saveSucess om allt funkar
             res.render('saveSucess',{thing:result}) 
             }
             });
     });
-
+ 
+         app.get('/api/photo', function(req,res){
+        res.render('testphoto');
+    });
+    app.post('/api/photo',function(req,res){
+        if(done=true){
+            console.dir(req.files);
+            console.log(req.files);
+            res.end("File uploaded");
+        }
+    });
+    //visar allt innehåll i databasen
     app.get('/sak', function(req,res){
         
         things.find().toArray(function(err, result){
@@ -34,21 +50,11 @@ module.exports = function(app,db){
                 res.send(err);
             }
             else{
+                console.log(result);
                 res.json(result);
             }
             });
     });
-        //Funkar inte...
-    app.get('/sak/:thing_id', function(req,res){
-        var findString = 'ObjectId("' + req.params.thing_id + '")';
-        things.find({_id:findString}, function(err,result){
-            if(err){
-                res.send(err);
-            }
-            else{
-                console.log(findString);
-                res.json(result);      
-            }
-        });
-    });
+    
+     
 };
--- Prototypes/4/server.js
+++ Prototypes/4/server.js
@@ -1,11 +1,12 @@
 //dependencies
     var express = require('express');
     var ejs = require('ejs');
-    var database = require('./config/database');
-//    var mongoose = require('mongoose');
+    var database = require('./config/database');                
     var routes = require('./app/routes');
     var bodyParser = require('body-parser');
     var mongodb = require('mongodb');
+    var multer = require('multer');
+
 
 //express
     var app = express();
@@ -14,18 +15,39 @@
     app.use(bodyParser.urlencoded({ 
         extended:true
     }));
-//load database-config
 
+//configure multer
+app.use(multer({dest:'./public/uploads/',
+                rename:function(fieldname,filename){
+                    console.log(fieldname);
+                    console.log(filename);
+                    return filename+Date.now();
+                },
+                onFileUploadStart:function(file){
+                    console.log(file.originalname + 'is starting ...')
+                },
+                onFileUploadComplete:function(file){
+                    console.log(file.fieldname + 'upladed to ' + file.path)
+                    done=true;
+                }
+               }));
+
+  
+                    
+                    
+                    
+//database-config
     //mongoose.connect(database.url);
 
-var MongoClient = mongodb.MongoClient;
+    var MongoClient = mongodb.MongoClient;
     MongoClient.connect(database.url,function(err,db){
         if(err){
             console.log('Unable to connect to mongoDB server.Error',err);
         }
         else{
             console.log('Connection established to ' + database.url);
-            routes(app,db);
+            routes(app,db); //laddar routes och skickar med databasen om anslutningen till
+                            //databasen funkar
         }
     });
 
