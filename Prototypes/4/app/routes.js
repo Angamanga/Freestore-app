17437a6591055a65a6294b6b64e24b4939d04d58
Angamanga <Angamanga@annas-mbp.lan>
2015-04-20 15:19:32 +0200

    cloudinary funkar :)

--- Prototypes/4/app/routes.js
+++ Prototypes/4/app/routes.js
@@ -1,60 +1,87 @@
-var bodyParser = require('body-parser');
-var multer = require('multer');
+var bodyParser = require('body-parser'),
+    cloudinary = require('cloudinary');
+var fileParser = require('connect-multiparty')();
+module.exports = function (app, db) {
 
-module.exports = function(app,db){
     //definierar collection things
-    
     var things = db.collection('things');
-    //visar index när man gör en get av /
-    app.get('/',function(req,res){
-       res.render('index');
+
+    //konfigurerar cloudinary //configure cloudinary
+    var cloudinaryCredentials = {
+        cloud_name: 'angamanga',
+        api_key: '579224836286857',
+        api_secret: '2a4eJXgPdjHSYtI1DRkvGIFq2c8'
+    };
+
+    cloudinary.config({
+        cloud_name: cloudinaryCredentials.cloud_name,
+        api_key: cloudinaryCredentials.api_key,
+        api_secret: cloudinaryCredentials.api_secret
     });
-    
+
+
+    //visar index när man gör en get av /
+    app.get('/', function (req, res) {
+        res.render('index');
+    });
+
     //visar newThing när man gör en get av /nysak
-    app.get('/nysak',function(req,res){
+    app.get('/nysak', function (req, res) {
         res.render('newThing');
     });
 
     //lägger till en ny sak i databasen när man gör en post på /nysak
-    app.post('/nysak',function(req,res){
-         var newThing = {
-            title:req.body.title,
-            description:req.body.description
-        };
-        things.insert(newThing, function(err, result){
-            if(err){
-                res.send(err);
+    app.post('/nysak', fileParser, function (req, res) {
+
+        //läser först in bild
+        var imageFile = req.files.image;
+        cloudinary.uploader.upload(imageFile.path, function (result) {
+            if (result.url) {
+                var newThing = {
+                    title: req.body.title,
+                    category: req.body.category,
+                    description: req.body.description,
+                    contact: {
+                        telephone: req.body.telephone,
+                        email: req.body.email
+                    },
+                    time: Date.now(),
+                    location: req.body.location,
+                    photopath: result.url
+                };
+                console.log(newThing.photopath);
+                things.insert(newThing, function (err, result) {
+                    if (err) {
+                        res.send(err);
+                    } else {
+                        //visar saveSucess om allt funkar
+                        res.render('saveSucess', {
+                            thing: result
+                        })
+                    }
+                });
+            } else {
+                console.log('error uploading to cloudinary: ', result);
+                res.send('did not get url');
             }
-            else{
-                //visar saveSucess om allt funkar
-            res.render('saveSucess',{thing:result}) 
-            }
-            });
+        });
+
+
+
     });
- 
-         app.get('/api/photo', function(req,res){
-        res.render('testphoto');
-    });
-    app.post('/api/photo',function(req,res){
-        if(done=true){
-            console.dir(req.files);
-            console.log(req.files);
-            res.end("File uploaded");
-        }
-    });
+
     //visar allt innehåll i databasen
-    app.get('/sak', function(req,res){
-        
-        things.find().toArray(function(err, result){
-            if(err){
+    app.get('/sak', function (req, res) {
+
+        things.find().toArray(function (err, result) {
+            if (err) {
                 res.send(err);
-            }
-            else{
+            } else {
                 console.log(result);
                 res.json(result);
             }
-            });
+        });
     });
-    
-     
+
+
 };
--- Prototypes/4/server.js
+++ Prototypes/4/server.js
@@ -1,60 +1,56 @@
 //dependencies
-    var express = require('express');
-    var ejs = require('ejs');
-    var database = require('./config/database');                
-    var routes = require('./app/routes');
-    var bodyParser = require('body-parser');
-    var mongodb = require('mongodb');
-    var multer = require('multer');
+var express = require('express');
+var ejs = require('ejs');
+var database = require('./config/database');
+var routes = require('./app/routes');
+var bodyParser = require('body-parser');
+var mongodb = require('mongodb');
+var cloudinary = require('cloudinary');
+var fileParser = require('connect-multiparty')();
 
 
 //express
-    var app = express();
-    app.use(express.static('public'));
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ 
-        extended:true
-    }));
+var app = express();
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-//configure multer
-app.use(multer({dest:'./public/uploads/',
-                rename:function(fieldname,filename){
-                    console.log(fieldname);
-                    console.log(filename);
-                    return filename+Date.now();
-                },
-                onFileUploadStart:function(file){
-                    console.log(file.originalname + 'is starting ...')
-                },
-                onFileUploadComplete:function(file){
-                    console.log(file.fieldname + 'upladed to ' + file.path)
-                    done=true;
-                }
-               }));
 
-  
-                    
-                    
-                    
+//configure cloudinary
+var cloudinaryCredentials = {
+    cloud_name:'angamanga', 
+    api_key:'579224836286857', 
+    api_secret:'2a4eJXgPdjHSYtI1DRkvGIFq2c8'
+};
+cloudinary.config({
+    cloud_name:cloudinaryCredentials.cloud_name,
+    api_key:cloudinaryCredentials.api_key,
+    api_secret:cloudinaryCredentials.api_secret
+});
+
+
+
+
 //database-config
-    //mongoose.connect(database.url);
+//mongoose.connect(database.url);
 
-    var MongoClient = mongodb.MongoClient;
-    MongoClient.connect(database.url,function(err,db){
-        if(err){
-            console.log('Unable to connect to mongoDB server.Error',err);
-        }
-        else{
-            console.log('Connection established to ' + database.url);
-            routes(app,db); //laddar routes och skickar med databasen om anslutningen till
-                            //databasen funkar
-        }
-    });
+var MongoClient = mongodb.MongoClient;
+MongoClient.connect(database.url, function (err, db) {
+    if (err) {
+        console.log('Unable to connect to mongoDB server.Error', err);
+    } else {
+        console.log('Connection established to ' + database.url);
+        routes(app, db); //laddar routes och skickar med databasen om anslutningen till
+        //databasen funkar
+    }
+});
 
 //view engine = ejs
-    app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
 
 //startar servern
-    app.listen(8080);
-    console.log('server has started');
+app.listen(8080);
+console.log('server has started');
